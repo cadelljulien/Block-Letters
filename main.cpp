4ac71216702a38318474b5112946f088f24abfc7
Cadell Julien <cjulien2019@fau.edu>
2022-05-12 11:29:52 -0400

    Readme and First block update

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<ctime> 
+
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C\n";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+std::cout << "C ";
+
